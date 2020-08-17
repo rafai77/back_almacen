366cdfce20ecb0988be5b7aa1156fed57bb21bf8
rafai77 <rafac11@hotmail.com>
2020-08-17 14:57:31 -0500

    tabla de usuiaros creada

--- src/BD.sql
+++ src/BD.sql
@@ -1,1 +1,15 @@
 --Consulatas y creacion de la base de datos;
+--entrar a la terminar y crear bd;
+
+create DATABASE alamacen;
+use mysql;
+use almacen
+ create TABLE usuarios
+ (
+    id_user INT NOT NULL PRIMARY KEY,
+    nombre varchar(50) NOT NULL,
+    user varchar(50) NOT NULL,
+    pass blob,
+    rol varchar(50) NOT NULL,
+    UNIQUE (user)
+ );
