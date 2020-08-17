f25923fd87236425288dc854610a8018d0bd0025
rafai77 <rafac11@hotmail.com>
2020-08-17 15:39:49 -0500

    encriptacion para la tabla de usuarios con key secreta listo

--- src/BD.sql
+++ src/BD.sql
@@ -6,10 +6,21 @@ use mysql;
 use almacen
  create TABLE usuarios
  (
-    id_user INT NOT NULL PRIMARY KEY,
+    id_user INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
     nombre varchar(50) NOT NULL,
     user varchar(50) NOT NULL,
     pass blob,
     rol varchar(50) NOT NULL,
     UNIQUE (user)
  );
+
+-- Insert rows into table 'usuarios'
+use almacen;
+INSERT INTO usuarios
+( 
+   nombre,user,pass,rol
+)
+VALUES
+("horticola cimarron","Hcimarron",aes_encrypt("cimarron2020","abc1234cimarron"),"admin");
+
+select user, aes_decrypt(pass ,"abc1234cimarron") from usuarios;
