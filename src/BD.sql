d37b0ecd53ca0828e0ccd19c43c15eb9f263c8f0
rafai77 <rafac11@hotmail.com>
2020-08-20 19:00:39 -0500

    se creo en la base la tabla inventario

--- src/BD.sql
+++ src/BD.sql
@@ -27,3 +27,30 @@ select user, aes_decrypt(pass ,"abc1234cimarron") from usuarios;
 
 
 select * from usuarios where user='Hcimarron' and aes_decrypt(pass ,"abc1234cimarron")='cimarron2020'
+
+
+ create TABLE inventario
+ (
+   id_producto  INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+   producto varchar(50) NOT NULL,
+   total int NOT NULL,
+   tipo varchar(50),
+   UNIQUE(producto)
+ );
+INSERT INTO inventario
+( 
+   producto,total,tipo
+)
+VALUES
+(
+    'Acido Sulfurico al 98%' ,5940, 'Liquido'
+);
+
+
+
+
+Acido Nitrico al 55%								
+Acido Sulfurico al 98%								
+Fitosal Raiz								
+Acido Fosforico Ambar al 85%								
+Bonosoil								
