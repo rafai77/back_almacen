40af1f0a8a337a8954bea977b538e4ddeb3b8e89
rafai77 <rafac11@hotmail.com>
2020-10-09 08:24:14 -0500

    v=Servidor

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 const express = require("express");
 const app = express();
 const jwt = require('jsonwebtoken'); //para la auth
